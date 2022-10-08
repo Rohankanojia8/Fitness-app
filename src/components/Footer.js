62ee33cf94f1f2d07d2e7b2a6bfbd95ad9c6bf3f
Rohankanojia8 <rohankanojia7080021@gmail.com>
2022-10-08 16:33:39 +0530

    fit commit

---
+++ src/components/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+
+const Footer = () => (
+  <Box 
+   mt="80px" 
+   bgcolor="#FFF3F4">
+    <Stack 
+     gap="40px" 
+     sx={{ alignItems: 'center' }} 
+     flexWrap="wrap" 
+     px="40px" 
+     pt="24px">
+      <img src='https://cdn-icons-png.flaticon.com/128/2964/2964551.png' alt="logo" style={{ width: '100px', height: '41px' }} />
+    </Stack>
+    <Typography 
+     variant="h5" 
+     sx={{ fontSize: { lg: '28px', xs: '20px' } }} 
+     mt="41px" 
+     textAlign="center" 
+     pb="40px">FITNESS CLUB</Typography>
+  </Box>
+);
+
+export default Footer;
